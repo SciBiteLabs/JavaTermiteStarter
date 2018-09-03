240d151897817c2286f9b5342caeebd0d5d012be
Simon White <simon@scibite.com>
2018-09-03 15:29:48 +0100

    Added API methods

--- src/main/java/com/scibite/hackathon/termite/TermiteService.java
+++ src/main/java/com/scibite/hackathon/termite/TermiteService.java
@@ -1,7 +1,14 @@
 package com.scibite.hackathon.termite;
 
+import com.scibite.termitej.common.hit.THit;
+
+import java.io.File;
+import java.util.List;
+
 /**
  * This class was created by simon on 03/09/2018.
  */
 public interface TermiteService {
+    List<THit> termite(String inputData) throws Exception;
+    List<THit> termite(File file) throws Exception;
 }
